e063ec3a2ecc31d229ea7882ea6254aa50d1002e
Weeraphat2000 <weeraphat.main@gmail.com>
2024-12-15 02:46:03 +0700

    update middleware

--- src/middlewares/test.ts
+++ src/middlewares/test.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class TestMiddleware implements NestMiddleware {
+  // parameter ก็คือ req, res, next ต้องเรียงตามนี้ ไม่เช่นนั้นจะ error  แต่ไม่ต้อง return อะไรกลับไป
   use(req: Request, res: Response, next: NextFunction) {
     console.log('...........testMiddleware..........');
     console.log(req.body, 'body');
